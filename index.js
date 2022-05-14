49dfbff9261b52ce974f577a6fdac1cab2044f44
Zachary Baumgarten <zach.baumgarten@yahoo.com>
2022-05-14 17:32:26 -0500

    adding 404 page route

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+
+require('dotenv').config()
+
 const { application } = require('express')
 const express = require('express')
 const app = express()
 
 application.get('/', (req, res) => {
-    res.send('Hhhhunnnnnggghhhhhh')
+    res.send('Hhhhunnnnnggghhhhhh!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 senpai not found 0w0</h1>')
+})
+
+app.listen(process.env.PORT)
