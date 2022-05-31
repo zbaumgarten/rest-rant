2eaacf6a92525f017e255c26123382ee74963c6c
Zachary Baumgarten <zach.baumgarten@yahoo.com>
2022-05-30 19:01:33 -0500

    Part 9: Seeding data, validations, and helper methods

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,19 +1,27 @@
 const React = require('react')
 const Def = require('../default')
 
-function show (data, index) {
+function show (data) {
     return (
         <Def>
             <main>
                 <h1>{ data.place.name }</h1>
-                <div>
-                    <p className="text-center">
-                        {data.place.cuisines}
-                    </p>
+                <div className="col-sm-6">
                     <img height="500px" src={data.place.pic} alt={data.place.name} />
-                    <p className="text-center">
-                        Located in {data.place.city}, {data.place.state}
-                    </p>
+                    <h3>
+                        Located in {data.place.city}, {data.place.state}.
+                    </h3>
+                </div>
+                <div className="col-sm-6">
+                    <h2>
+                        Description
+                    </h2>
+                    <h3>
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4>
+                        Serving {data.place.cuisines}
+                    </h4>
                 </div>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                     Edit
